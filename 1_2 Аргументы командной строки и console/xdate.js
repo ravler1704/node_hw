f2a5235f054ddf4e12418fe87394b8953e23c91e
Alexandr <raskatov.av4@mipt.ru>
2024-04-01 10:10:31 +0300

    fix 1_2

---
+++ 1_2 Аргументы командной строки и console/xdate.js
@@ -0,0 +1,108 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+
+const now = new Date();
+let isSuccess = false;
+
+function getDay(date) {
+  try {
+    return date.getDate();
+  } catch (error) {
+    //
+  }
+}
+
+function getMonth(date) {
+  try {
+    return date.getMonth();
+  } catch (error) {
+    //
+  }
+}
+
+function calculateDate(date, d = 0, m = 0, y = 0, mode = 'add') {
+  try {
+    const day = date.getDate() + (mode === 'add' ? d : -d);
+    const month = date.getMonth() + (mode === 'add' ? m : -m);
+    const year = date.getFullYear() + (mode === 'add' ? y : -y);
+
+    return new Date(year, month, day).toISOString();
+  } catch (error) {
+    //
+  }
+}
+
+yargs(hideBin(process.argv))
+  .command(
+    'current',
+    'Текущая дата и время в формате ISO',
+    (yargsInner) => getOptions(yargsInner, 'boolean', 'Текущий'),
+    (argv) => {
+      getCurrentDate(argv);
+      isSuccess = true;
+    },
+  )
+  .command(
+    'add',
+    'Будущая дата и время в формате ISO',
+    (yargsInner) => getOptions(yargsInner, 'number', 'Будующий'),
+    (argv) => {
+      setDate(argv, 'add');
+      isSuccess = true;
+    },
+  )
+  .command(
+    'sub',
+    'Прошедшая дата и время в формате ISO',
+    (yargsInner) => getOptions(yargsInner, 'number', 'Прошлый'),
+    (argv) => {
+      setDate(argv, 'sub');
+      isSuccess = true;
+    },
+  )
+  .argv;
+
+if (!isSuccess) {
+  console.log('Такой команды не существует! Воспользуйтесь --help дя поучения дополнительной информации');
+}
+
+function getOptions(yargsInner, type, descriptionKey) {
+  return yargsInner
+    .option('y', {
+      'alias': 'year',
+      description: `${descriptionKey} год`,
+      type,
+    })
+    .option('m', {
+      'alias': 'month',
+      description: `${descriptionKey} месяц`,
+      type,
+    })
+    .option('d', {
+      'alias': 'date',
+      description: `${descriptionKey} день`,
+      type,
+    });
+}
+
+function getCurrentDate(argv = {}) {
+  const {date, month, year} = argv;
+
+  if (!date && !month && !year) console.log(now.toISOString());
+  if (date) console.log(getDay(now));
+  if (month) console.log(getMonth(now));
+  if (year) console.log(now.getFullYear());
+}
+
+function setDate(argv = {}, mode) {
+  const {date, month, year} = argv;
+
+  if (!date && !month && !year) {
+    console.log('Отсутствует параметр. Введите --help для получения документации');
+    return;
+  }
+
+  console.log(calculateDate(now, date, month, year, mode));
+}
